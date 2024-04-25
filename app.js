44a4ab576b2700244e0a6f3bfba7fcb0cf2eb025
Daniel Guerrero <daguerre@gmail.com>
2024-04-25 15:35:02 -0400

    Commit Inicial

---
+++ app.js
@@ -0,0 +1,14 @@
+require('dotenv').config();  // Busca la variable en .env (variables solo de desarollo)
+const port = process.env.PORT || 3000; // Busca la variable en .env (variables solo de desarollo)
+const aux = 1;
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hola Mundo!');
+});
+
+app.listen(port, () => {
+  console.log('Aplicación escuchando en el puerto '+port);
+});
